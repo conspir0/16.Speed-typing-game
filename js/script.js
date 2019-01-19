2c3dd8128a5b90a9b840349eaee35cda44425892
Mateusz Michalczyk <pan.michalczyk@gmail.com>
2019-01-19 14:51:19 +0100

    Add countdown function, checkStatus function, startMatch function, matchWords function, avialable levels

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 window.addEventListener('load', init);
 
 // Globals
-let time = 5,
+
+// Available levels
+const levels = {
+    easy: 5,
+    medium: 3,
+    hard: 1
+}
+
+// To change level
+const currentLevel = levels.easy;
+
+let time = currentLevel,
     score = 0,
     isPlaying;
 
@@ -22,15 +33,85 @@ const words = [
 
 // Initialize Game
 function init() {
+    
+    // Show number of seconds in UI
+    seconds.innerHTML = currentLevel;
+    
     // Load word from array
     showWord(words);
+    
+    // Start matching on input
+    wordInput.addEventListener('input', startMatch);
+    
+    // Call countdown every second
+    setInterval(countdown, 1000);
+    
+    // Check game status
+    setInterval(checkStatus, 50);
+}
+
+// Start match
+function startMatch() {
+    if(matchWords()) {
+        isPlaying = true;
+        time = currentLevel + 1;
+        showWord(words);
+        wordInput.value = '';
+        score++;
+    }
+    
+    // If score is -1, display 0
+    if(score === -1) {
+        scoreDisplay.innerHTML = 0;
+    } else {
+        scoreDisplay.innerHTML = score;
+    }
+}
+
+// Match currentWord to wordInput
+function matchWords() {
+    if(wordInput.value === currentWord.innerHTML) {
+        message.innerHTML = 'Correct!';
+        return true;
+    } else {
+        message.innerHTML = '';
+        return false;
+    }
 }
 
 // Pick & show random word
 function showWord(words) {
+    
     // Generate random array index
     const ranIndex = Math.floor(Math.random() * words.length);
     
-    //Output random word
+    // Output random word
     currentWord.innerHTML = words[ranIndex];
 }
+
+// Countdown timer
+function countdown() {
+    
+    // Make sure time is not run out
+    if(time > 0) {
+        
+        // Decrement
+        time--;
+    } else if(time === 0) {
+        
+        // Game is over
+        isPlaying = false;
+    }
+    
+    // Show time
+    timeDisplay.innerHTML = time;
+}
+
+// Check game status
+function checkStatus() {
+    if(!isPlaying && time === 0) {
+        message.innerHTML = 'Game Over!';
+        score = -1;
+    }
+}
+
